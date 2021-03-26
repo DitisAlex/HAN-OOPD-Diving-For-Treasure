7270f696d2ade9750dcde3e1cd6c640c8147a95c
DitisAlex <alexcheng0314@gmail.com>
2021-03-26 13:26:48 +0100

    Moved src file to own directory

--- src/main/java/project/DivingForTreasure/screen/Screen.java
+++
@@ -1,7 +0,0 @@
-package project.DivingForTreasure.screen;
-
-public abstract class Screen {
-    // Abstract Functions
-    public abstract void spawnScreenObjects();
-    public abstract void deleteScreenObjects();
-}
